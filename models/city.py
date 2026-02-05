2ee2d6d907789bbfd455cadb90d609216f78b15d
L-nsamba <l.nsamba@alustudent.com>
2026-02-05 16:56:59 +0200

    updates

--- models/city.py
+++ models/city.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """The city class, contains state ID and name"""
+    """The city class, contains state ID"""
+
     __tablename__ = "cities"
 
-    name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+
+    # Relationship with Place
+    places = relationship(
+        "Place",
+        backref="cities",
+        cascade="all, delete, delete-orphan"
+    )
--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -13,3 +14,10 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    # Relationship with Place
+    places = relationship(
+        "Place",
+        backref="user",
+        cascade="all, delete, delete-orphan"
+    )
